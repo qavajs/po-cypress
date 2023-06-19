bfc7f4e19a19f5c94bf690daa211f5551df844a7
Oleksandr_Halichenko <oleksandr_halichenko@epam.com>
2023-06-19 14:24:00 +0300

    updated gitignore and d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,15 +1,14 @@
-import { Locator } from 'playwright';
 declare function $(selector: string|Object, options?: { ignoreHierarchy: boolean }): Object;
 declare function $$(selector: string|Object, options?: { ignoreHierarchy: boolean }): Object;
 declare type PageObject = {
     init(driver, options: { timeout: number }): void;
     register(pageObject: Object): void;
-    getElement(path: string): Locator
+    getElement(path: string): any
 }
 declare let po: PageObject;
 declare class Component {
     constructor(selector?: string | object)
 }
-declare module '@qavajs/po-playwright' {
+declare module '@qavajs/po-cypress' {
     export { $, $$, po, Component }
 }
